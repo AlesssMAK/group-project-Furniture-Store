af5d6eb06f74e097f672dc1b6c1800a8283dda08
Alexey <makovii88@gmail.com>
2025-08-05 22:47:23 +0200

    update sprite

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,1 +1,21 @@
+// Імпорт стилів (обов’язково)
+import 'swiper/css';
+
+// Імпорт самого Swiper і модулів, якщо треба
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// Ініціалізація
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
 console.log("feedback");
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./js/header-menu.js";
 import "./js/furniture-list.js";
+import "./js/faq.js";
 import "./js/feedback";
 import "./js/furniture-details-modal";
 import "./js/order-modal.js";
