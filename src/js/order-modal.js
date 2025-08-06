c1e82c431e0b358786d9abfe973a5157c25f8a47
Mila <malaksumaaa@gmail.com>
2025-08-07 00:20:12 +0300

    add js btn close

--- src/js/order-modal.js
+++ src/js/order-modal.js
@@ -1,1 +1,33 @@
-console.log("order-modal");
+import iziToast from "izitoast";
+    import { createOrder } from "./furniture-store-api";
+
+const btnClose = document.querySelector('.modal-window-close-btn');
+const orderModal = document.querySelector ('.order-modal');
+const formModalOrder = document.querySelector ('.modal-window-form')
+
+function openModal() {
+  orderModal.style.display = "block";
+  document.body.style.overflow = "hidden";
+}
+
+btnClose.addEventListener ("click",closeModalOrder);
+
+btnClose.addEventListener ("keydown", (event) => {
+    if (event.key === escape) {
+        closeModalOrder()
+    }
+});
+
+btnClose.addEventListener ("click", (event) => {
+    if (event.target === orderModal) {
+        closeModalOrder();
+    }
+})
+
+function closeModalOrder() {
+orderModal.style.displey = "none";
+document.body.style.overflow = "";
+}
+
+
+createOrder();
