42e50bbd0ea6f8927a0be2fa72d4fb6cbe37f6e7
AlesssMAK <makovii88@gmail.com>
2025-08-06 15:06:11 +0200

    Update furniture-store-api.js

--- src/js/furniture-store-api.js
+++ src/js/furniture-store-api.js
@@ -1,9 +1,1 @@
-import axios from "axios";
-import { BASE_URL, ENDPOINTS } from "./constants";
 
-axios.defaults.baseURL = BASE_URL;
-
-export const fetchFurniture = async () => {
-    const {data} = await axios(`${ENDPOINTS.FORNITURES}?page=1&limit=8`);
-    return data;
-}
