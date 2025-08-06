2af5b2091a0622cea4045a7862f2f39f91d8ce0f
Evgeniy Vovk <evgeniy.vovk@ukr.net>
2025-08-06 22:36:08 +0300

    add loader

---
+++ src/js/loader.js
@@ -0,0 +1,9 @@
+const loader = document.getElementById('loader');
+
+export function showLoader() {
+  if (loader) loader.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  if (loader) loader.classList.add('hidden');
+}
