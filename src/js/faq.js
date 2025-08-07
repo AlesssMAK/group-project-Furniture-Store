1df2676f89c3f11e52e76f56a43fc3eb6afb3934
Roman <pavlyukroman9@gmail.com>
2025-08-07 18:34:10 +0300

    HTML CSS JS

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,2 +1,11 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.accordion-container', {
+  duration: 250,
+  showMultiple: false,
+
+  onOpen: function (currentElement) {
+    console.log(currentElement);
+  },
+});
